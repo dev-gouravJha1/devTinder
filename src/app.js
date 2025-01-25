9515331c8110e67da7e1db53071c9673bf13f584
Gourav Jha <gouravjha297@gmail.com>
2025-01-25 23:18:21 +0530

    exploring routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
 
-app.use("/getData",(req,res)=>{
-    res.send("Hll")
+// app.use("/getData",(req,res)=>{
+//     res.send("Hll")
+// })
+app.get("/user",(req,res)=>{
+    res.send({name:"Gourav",lastName:"Jha"})
+})
+app.post("/user",(req,res)=>{
+    res.send("Data updated")
+})
+app.put("/user",(req,res)=>{
+    res.send("data update successfuly")
+})
+app.delete("/user",(req,res)=>{
+    res.send("Data delete successfuly")
 })
 
 app.listen(3000,()=>{
-    console.log("Server is runnin")
+    console.log("Server is running")
 })
