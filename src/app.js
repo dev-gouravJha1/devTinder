ee9969257d5b1a56ee8c9c1deb77df6b73e1d4a2
= <=>
2025-01-25 15:05:55 +0530

    first code

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.use("/getData",(req,res)=>{
+    res.send("Hll")
+})
+
+app.listen(3000,()=>{
+    console.log("Server is running")
+})
