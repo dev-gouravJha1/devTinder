37f75193bc1bce0422d45521280171e344e47eae
Gourav Jha <gouravjha297@gmail.com>
2025-01-28 08:35:50 +0530

    new example of auth

--- src/app.js
+++ src/app.js
@@ -1,21 +1,33 @@
 const express = require("express");
 const app = express();
 
-// app.use("/getData",(req,res)=>{
-//     res.send("Hll")
-// })
-app.get("/user",(req,res)=>{
+app.use("/",(req,res,next)=>{
+   //here i will authenticate
+   const auth = "xyz"
+   if(auth=="xyz"){
+    next()
+   }
+   else{
+    res.statusCode(401)
+   }
+})
+app.get("/user/addUser",(req,res,next)=>{
+    console.log("REQUESTDATA",req.params)
     res.send({name:"Gourav",lastName:"Jha"})
 })
-app.post("/user",(req,res)=>{
-    res.send("Data updated")
+app.get("/user/removeuser",(req,res,next)=>{
+    res.send("User removed")
 })
-app.put("/user",(req,res)=>{
-    res.send("data update successfuly")
-})
-app.delete("/user",(req,res)=>{
-    res.send("Data delete successfuly")
-})
+
+// app.post("/user",(req,res)=>{
+//     res.send("Data updated")
+// })
+// app.put("/user",(req,res)=>{
+//     res.send("data update successfuly")
+// })
+// app.delete("/user",(req,res)=>{
+//     res.send("Data delete successfuly")
+// })
 
 app.listen(3000,()=>{
     console.log("Server is running")
